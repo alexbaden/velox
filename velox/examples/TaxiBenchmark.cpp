b61889ca99bd0e8d40dd9890a114dd9d20fa3ddf
Alex Baden <alexander.baden@intel.com>
2022-10-21 14:37:03 -0500

    Taxi Benchmark 2/? : Support queries via hive

--- velox/examples/TaxiBenchmark.cpp
+++ velox/examples/TaxiBenchmark.cpp
@@ -3,8 +3,13 @@
 #include <folly/init/Init.h>
 
 #include "velox/common/file/FileSystems.h"
+#include "velox/connectors/hive/HiveConnector.h"
+#include "velox/connectors/hive/HiveConnectorSplit.h"
+#include "velox/dwio/dwrf/reader/DwrfReader.h"
 #include "velox/dwio/parquet/RegisterParquetReader.h"
 #include "velox/dwio/parquet/reader/ParquetReader.h"
+#include "velox/exec/Task.h"
+#include "velox/exec/tests/utils/PlanBuilder.h"
 #include "velox/vector/BaseVector.h"
 
 using namespace facebook::velox;
@@ -18,6 +23,17 @@ int main(int argc, char** argv) {
     std::cout << "No file path provided" << std::endl;
     return 1;
   }
+
+  // We need a connector id string to identify the connector.
+  const std::string kHiveConnectorId = "test-hive";
+
+  // Create a new connector instance from the connector factory and register
+  // it:
+  auto hiveConnector =
+      connector::getConnectorFactory(
+          connector::hive::HiveConnectorFactory::kHiveConnectorName)
+          ->newConnector(kHiveConnectorId, nullptr);
+  connector::registerConnector(hiveConnector);
 
   // To be able to read local files, we need to register the local file
   // filesystem. We also need to register the parquet reader factory:
@@ -36,14 +52,30 @@ int main(int argc, char** argv) {
     return 1;
   }
   std::cout << "Read " << *reader->numberOfRows() << " rows." << std::endl;
+  auto inputRowType = reader->rowType();
 
-  VectorPtr batch;
-  RowReaderOptions rowReaderOptions;
-  auto rowReader = reader->createRowReader(rowReaderOptions);
-  while (rowReader->next(500, batch)) {
-    auto rowVector = batch->as<RowVector>();
-    for (vector_size_t i = 0; i < rowVector->size(); ++i) {
-      std::cout << rowVector->toString(i) << std::endl;
+  core::PlanNodeId scanNodeId;
+  auto readPlanFragment = exec::test::PlanBuilder()
+                              .tableScan(inputRowType)
+                              .capturePlanNodeId(scanNodeId)
+                              .topN({"trip_id"}, 5, /*isPartial=*/false)
+                              .planFragment();
+
+  auto readTask = std::make_shared<exec::Task>(
+      "my_read_task",
+      readPlanFragment,
+      /*destination=*/0,
+      core::QueryCtx::createForTest());
+
+  auto connectorSplit = std::make_shared<connector::hive::HiveConnectorSplit>(
+      kHiveConnectorId, "file:" + filePath, dwio::common::FileFormat::PARQUET);
+  readTask->addSplit(scanNodeId, exec::Split{connectorSplit});
+  readTask->noMoreSplits(scanNodeId);
+
+  while (auto result = readTask->next()) {
+    LOG(INFO) << "Vector available after processing (scan + sort):";
+    for (vector_size_t i = 0; i < result->size(); ++i) {
+      LOG(INFO) << result->toString(i);
     }
   }
 
